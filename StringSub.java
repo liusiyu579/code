77993a6ff32fc0a8a88730278a9014bf8471de3e
liusiyu579 <969529245@qq.com>
2020-06-27 10:40:35 +0800

    {6,-3,-2,7,-15,1,2,2},连续子向量的最大和为8(从第0 个开始,到第3个为止)。给一个数组，返回它的最大连续子序列的和

--- StringSub.java
+++ StringSub.java
@@ -1,5 +1,6 @@
-public class MaxStringSub {
-	//
+//{6,-3,-2,7,-15,1,2,2},连续子向量的最大和为8(从第0
+//个开始,到第3个为止)。给一个数组，返回它的最大连续子序列的和
+public class StringSub {
     public int FindGreatestSumOfSubArray(int[] array) {
         int max = array[0];
         int cur = array[0];
